b265304092bde9c179f3467fd9ee051131573cce
David Shin <dshin1@cpp.edu>
2017-09-28 04:43:05 -0700

    EchoServer and EchoClient working.

--- EchoClient.java
+++ EchoClient.java
@@ -1,31 +1,85 @@
-// ============================================================================
-// file: EchoClient.java
-// ============================================================================
-// Programmer: David Shin
-// Date: 09/26/2017
-// Class: CS 380("Computer Networks")
-// Time: TTH 3:00pm
-// Instructor: Mr. Davarpanah
-// Assignment: 01
-//
-// Description:
-//      
-//
-// ============================================================================
-
-import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
-public final class EchoClient {
+public class EchoClient
+{
 
-    public static void main(String[] args) throws Exception {
-	try (Socket socket = new Socket("localhost", 22222)) {
-	    InputStream is = socket.getInputStream();
-	    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-	    BufferedReader br = new BufferedReader(isr);
-	    System.out.println(br.readLine());
-	}
-    }
-}
+    private static Socket socket;
+
+    public static void main(String args[])
+    {
+	Scanner sc = new Scanner(System.in);
+
+	while(true)
+	{
+	    try
+	    {
+		       
+			Socket socket = new Socket("localhost", 22222);
+			//Send the message to the server
+			OutputStream os = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+
+						//Get the return message from the server
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+
+			String number, sendMessage, message;
+			
+			System.out.print("Client> ");
+			number = sc.nextLine();
+			
+			if(number.equals("exit"))
+			   {
+			       try{ socket.close();
+				   return;}
+			       catch(Exception e)
+				   {e.printStackTrace();
+				   }
+			   }
+			
+			sendMessage = number + "\n";
+			bw.write(sendMessage);
+			bw.flush();
+
+			//System.out.println("Message sent to the server: " + sendMessage);
+			message = br.readLine();
+			System.out.println("Server> " + message);
+   
+	    }//end try block
+			
+	
+
+    
+	catch (Exception exception)
+	    {
+		exception.printStackTrace();
+	    }
+
+	}//end while loop
+
+	/*
+	finally
+	    {
+		//Closing the socket
+	            try
+			{
+			    //    socket.close();
+			}
+		    catch(Exception e)
+			{
+			    e.printStackTrace();
+			}
+	    }//end finally
+	*/
+    }//end main
+}//end class
--- EchoServer.java
+++ EchoServer.java
@@ -1,34 +1,79 @@
-// ============================================================================
-// file: EchoServer.java
-// ============================================================================
-// Programmer: David Shin
-// Date: 09/26/2017
-// Class: CS 380("Computer Networks")
-// Time: TTH 3:00pm
-// Instructor: Mr. Davarpanah
-// Assignment: 01
-//
-// Description:
-//    
-// ============================================================================
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public final class EchoServer {
+public class EchoServer
+{
 
-    public static void main(String[] args) throws Exception {
-	try (ServerSocket serverSocket = new ServerSocket(22222)) {
-	    while (true) {
-		try (Socket socket = serverSocket.accept()) {
-		    String address = socket.getInetAddress().getHostAddress();
-		    System.out.printf("Client connected: %s%n", address);
-		    OutputStream os = socket.getOutputStream();
-		    PrintStream out = new PrintStream(os, true, "UTF-8");
-		    out.printf("Hi %s, thanks for connecting!%n", address);
-		}
+    private static Socket socket;
+   
+    public static void main(String[] args) throws Exception
+    {
+	    
+	
+	try (ServerSocket serverSocket = new ServerSocket(22222))
+	    {
+		int temp = 1;
+		//Server is running always. This is done using this while(true) loop
+		while(true)
+		    {
+			try
+			{
+			Socket socket = serverSocket.accept();
+			String address = socket.getInetAddress().getHostAddress();
+			if(temp == 1)
+			{
+			    System.out.printf("Client connected: %s%n", address);
+			    temp--;
+			}
+
+			//Reading the message from the client
+			//socket = serverSocket.accept();
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String number = br.readLine();
+			//System.out.println("Message received from client is "+number);
+			if (number == null)
+			    {
+				System.out.println("Client disconnected: " + address);
+			     
+			    }
+			String returnMessage = number + '\n';
+				
+
+			//Sending the response back to the client.
+			OutputStream os = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+			bw.write(returnMessage);
+			//System.out.println("Message sent to the client is "+returnMessage);
+			bw.flush();
+			}//end inner-try
+		       	catch (Exception e)
+			    {
+				e.printStackTrace();
+			    }//end inner try-catch block
+
+	       }//end while
+	    }//end first try
+	    catch (Exception e)
+	    {
+		e.printStackTrace();
 	    }
-	}
+	finally
+	    {
+	        try
+		{
+		    socket.close();
+		}
+		catch(Exception e){}
+	    }
     }
 }
+			      
